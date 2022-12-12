bf6192b9698f917c0fcf6b47ecc60d9219ded21f
colaruo <colruo0404@gmail.com>
2022-12-12 10:18:44 +0800

    2022-12-12-2

--- exercise5.py
+++ exercise5.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 x = range(1, 9)
 data1 = [1, 4, 9, 16, 25, 36, 49, 64]
 data2 = [1, 2, 3, 6, 9, 15, 24, 39]
+data3 = [2, 4, 6]
 
 plt.plot(x, data1, "--", color = "b", linewidth = 1, marker = "o", markersize = 3)
 plt.plot(x, data2, "--", color = "r", linewidth = 1, marker = "o", markersize = 3)
